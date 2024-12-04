01275cbe362212114092e1ba48513adc26aaa0ff
w3cdp6084-dev <yusukew3@gmail.com>
2024-12-04 11:34:28 +0900

    fontchange

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -28,6 +28,12 @@ export default defineNuxtConfig({
           `,
           type: 'text/javascript'
         }
+      ],
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&family=Poppins:wght@400;500;600;700&display=swap'
+        }
       ]
     }
   },
